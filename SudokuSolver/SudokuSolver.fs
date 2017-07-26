00ad406d625e27409f6a628baf81bd284c092a55
Imran Khaliq <ikhaliq42@gmail.com>
2017-07-26 23:08:24 +0100

    Some refactoring and removal of smaller unit tests

--- SudokuSolver/SudokuSolver.fs
+++ SudokuSolver/SudokuSolver.fs
@@ -1,81 +1,98 @@
 ﻿module Solver
 
-let gridSize = 9
-let subGridsize = 3
-
-// function to get row and column of flattened puzzle grid
-let rowAndCol idx = 
-    let r = idx / gridSize + 1
-    let c = (idx+1) - (r-1) * gridSize
-    r,c
-
 // convert a string of digits representing the puzzle to a list of ints
-let intStringToList intString = 
-    intString |> Seq.map (string >> int) |> Seq.toList;; 
+let intStringToList intString = intString |> Seq.map (string >> int) |> Seq.toList 
 
 // convert a list of ints to a string
 let intListToString intList = intList |> List.map (fun i -> i.ToString()) |> String.concat ""
 
-// create initial solved set from flattened puzzle set
-let intialSolvedSet (puzzleString: string) =
-    let flatPuzzle = intStringToList puzzleString
-    [ for i,k in (flatPuzzle |> List.indexed) do if k <> 0 then yield (rowAndCol i), k ]
-    |> Map.ofList
-
-// create initial unsolved set from flattened puzzle set
-let intialUnsolvedSet (puzzleString: string) =   
-    let flatPuzzle = intStringToList puzzleString 
-    [ for i,k in (flatPuzzle |> List.indexed) do if k = 0 then yield rowAndCol i]
-    |> Set.ofList
-
-// check if cell i,j is in the same sub-grid as cell r,c
-let sameSubGrid (r, c) (i,j) =
-    let s = subGridsize
-    let m = (r-1) / s * s + 1
-    let n = (c-1) / s * s + 1 
-    [ for i in m .. (m+s-1) do for j in n .. (n+s-1) -> (i,j) ]
-    |> List.contains (i,j)
-
-// find all solved values in the same row, column or sub-grid as cell r,c
-// but not r,c itself
-let cannotBe r c solutionSet =
-    [ for (i,j),k in solutionSet |> Map.toList do 
-        if (i = r || j = c || sameSubGrid (r,c) (i,j)) && (i,j) <> (r,c) then yield k ]
-
-// check if an unsolved cell has a solution, and return solution if found, 
-// or zero otherwise
-let cellSoln r c solutionSet =
-    let possibleValues = [1 .. gridSize] |> List.except (cannotBe r c solutionSet)
-    if possibleValues |> List.length = 1 then  
-        possibleValues |> List.exactlyOne
-    else  
-        0
-
-// go through unsolved set and solve any cells that can be solved, 
-// and add newly solved cells to solved set
-let sweep (solvedSet, unsolvedSet) =
-
-    let newSolvedSet = [ for r,c in unsolvedSet do yield (r,c),(cellSoln r c solvedSet) ]
-                       |> List.filter(fun (i,k) -> k <> 0)
-                       |> List.append (solvedSet |> Map.toList)
-                       |> Map.ofList
-
-    let newUnsolvedSet = [ for r,c in unsolvedSet do 
-                                if not (newSolvedSet |> Map.containsKey (r,c)) then yield r,c ] 
-                         |> Set.ofList
-
-    newSolvedSet, newUnsolvedSet
-
 // calculate the solution and return as a list
-let solvePuzzle puzzle = 
+let solution puzzle = 
+
+    // function to get row and column of flattened puzzle grid
+    let rowAndCol gridSize idx = 
+        let r = idx / gridSize + 1
+        let c = (idx+1) - (r-1) * gridSize
+        r,c
+    
+    // get grid size of puzzle
+    let gridSize = int (sqrt (float (String.length puzzle)))
+
+    // get sub grid size of puzzle
+    let subGridsize = int (sqrt (float (gridSize)))
+
+    // check if cell i,j is in the same sub-grid as cell r,c
+    let sameSubGrid (r, c) (i,j) =
+        let s = subGridsize
+        let m = (r-1) / s * s + 1
+        let n = (c-1) / s * s + 1 
+        [ for i in m .. (m+s-1) do for j in n .. (n+s-1) -> (i,j) ]
+        |> List.contains (i,j)
+
+    // create initial solved set from flattened puzzle set
+    let intialSolvedSet (puzzleString: string) =
+        let flatPuzzle = intStringToList puzzleString
+        [ for i,k in (flatPuzzle |> List.indexed) do if k <> 0 then yield (rowAndCol gridSize i), k ]
+        |> Map.ofList
+
+    // create initial unsolved set from flattened puzzle set
+    let intialUnsolvedSet (puzzleString: string) =   
+        let flatPuzzle = intStringToList puzzleString 
+        [ for i,k in (flatPuzzle |> List.indexed) do if k = 0 then yield rowAndCol gridSize i]
+        |> Set.ofList
+    // find all solved values in the same row, column or sub-grid as cell r,c
+    // but not r,c itself
+    let cannotBe r c solutionSet =
+        [ for (i,j),k in solutionSet |> Map.toList do 
+            if (i = r || j = c || sameSubGrid (r,c) (i,j)) && (i,j) <> (r,c) then yield k ]
+
+    // check if an unsolved cell has a solution, and return solution if found, 
+    // or zero otherwise
+    let cellSoln r c solutionSet =
+        let possibleValues = [1 .. gridSize] |> List.except (cannotBe r c solutionSet)
+        if possibleValues |> List.length = 1 then  
+            possibleValues |> List.exactlyOne
+        else  
+            0
+
+    // go through unsolved set and solve any cells that can be solved, 
+    // and add newly solved cells to solved set
+    let sweep (solvedSet, unsolvedSet) =
+        let newSolvedSet = [ for r,c in unsolvedSet do yield (r,c),(cellSoln r c solvedSet) ]
+                           |> List.filter(fun (i,k) -> k <> 0)
+                           |> List.append (solvedSet |> Map.toList)
+                           |> Map.ofList
+        let newUnsolvedSet = [ for r,c in unsolvedSet do 
+                                    if not (newSolvedSet |> Map.containsKey (r,c)) then yield r,c ] 
+                             |> Set.ofList
+        newSolvedSet, newUnsolvedSet
 
     let rec solver (solvedSet, unsolvedSet) =
-        if unsolvedSet |> Set.isEmpty then 
+       if unsolvedSet |> Set.isEmpty then 
             (solvedSet, unsolvedSet)
         else
             solver (sweep (solvedSet, unsolvedSet))
     
     let solvedSet, unsolvedSet = solver (intialSolvedSet puzzle, intialUnsolvedSet puzzle)
-    let flatSoln = [ for r in 1 .. gridSize do 
-                        for c in 1 .. gridSize do yield solvedSet |> Map.find (r,c) ]
-    intListToString flatSoln
+
+    [ for r in 1 .. gridSize do for c in 1 .. gridSize do yield solvedSet |> Map.find (r,c) ]
+
+
+let solvePuzzle puzzle =
+    
+    // check if puzzle is a valid size - must be a perfect square and perfect cube
+    let isValidSize puzzle =    
+        let l = String.length puzzle
+        let square_root = sqrt (float l)
+        let cube_root = sqrt (square_root)
+        let square_root_trunc =  truncate square_root
+        let cube_root_trunc = truncate cube_root
+        if square_root = square_root_trunc && cube_root = cube_root_trunc then true else false
+
+    // check that puzzle is valid - various tests can be added here
+    let isValidPuzzle = isValidSize puzzle
+
+    if isValidPuzzle then 
+        intListToString (solution puzzle)
+    else 
+        "Error: Invalid puzzle."
--- SudokuSolver/SudokuSolverTests.fs
+++ SudokuSolver/SudokuSolverTests.fs
@@ -7,105 +7,16 @@ open FSharp.Data
 type Puzzle = CsvProvider<"sudoku.csv">
 
 [<Test>]
-let ``Test rowAndCol`` () =
-    Assert.AreEqual((1,1), rowAndCol 0)
-    Assert.AreEqual((1,6), rowAndCol 5)
-    Assert.AreEqual((3,6), rowAndCol 23)
-    Assert.AreEqual((9,9), rowAndCol 80)
-
-[<Test>]
-let ``Test intStringToList`` () =
-    let func = intStringToList
-    let input = "61971"
-    let expectedOutput = [6;1;9;7;1]
-    let actualOutput = func input 
-    Assert.AreEqual(expectedOutput, actualOutput)
-
-[<Test>]
-let ``Test intListToString`` () =
-    let func = intListToString
-    let input = [6;1;9;7;1]
-    let expectedOutput = "61971"
-    let actualOutput = func input 
-    Assert.AreEqual(expectedOutput, actualOutput)
-
-[<Test>]
-let ``Test intialSolvedSet`` () =
-    let func = intialSolvedSet
-    let input = "004300000000000000000000000000000000000000000000000000000000000000090000000000005"
-    let expectedOutput = [((1,3),4);((1,4),3);((8,5),9);((9,9),5)] |> Map.ofList
-    let actualOutput = func input 
-    Assert.AreEqual(expectedOutput, actualOutput)
-
-[<Test>]
-let ``Test intialUnsolvedSet`` () =
-    let func = intialUnsolvedSet
-    let input = "120056789123456789123456789123456789123456789123456789123456789123406789123456780"
-    let expectedOutput = [(1,3);(1,4);(8,5);(9,9)] |> Set.ofList
-    let actualOutput = func input 
-    Assert.AreEqual(expectedOutput, actualOutput)
-
-[<Test>]
-let ``Test sameSubGrid`` () =
-    let func = sameSubGrid
-    let input = [(1,1);(4,4);(7,8);(9,9);(6,6)] 
-                |> List.zip [(3,3);(2,7);(7,8);(9,7);(3,7)]
-    let expectedOutput = [true;false;true;true;false]
-    let actualOutput = [for i1,i2 in input do yield func i1 i2 ]
-    Assert.AreEqual(expectedOutput, actualOutput)
-
-[<Test>]
-let ``Test cannotBe`` () =
-    let func = cannotBe
-    let ((r,c),s) = ((5,5),[((5,1),1);((2,5),2);((4,4),3);((2,2),4)] |> Map.ofList)
-    let expectedOutput = [1;2;3]    
-    let actualOutput = func r c s |> List.sort
-    Assert.AreEqual(expectedOutput, actualOutput)
-
-[<Test>]
-let ``Test cellSoln when no solution`` () =
-    let func = cellSoln
-    let ((r,c),s) = ((5,5),[((5,1),1);((2,5),2);((4,4),3);((2,2),4)] |> Map.ofList)
-    let expectedOutput = 0    
-    let actualOutput = cellSoln r c s
-    Assert.AreEqual(expectedOutput, actualOutput)
-
-[<Test>]
-let ``Test cellSoln when is a solution`` () =
-    let func = cellSoln
-    let cells = [(5,1);(5,2);(5,7);(5,9);(1,5);(3,5);(4,5);(8,5);(2,2)]
-    let vals = [1;2;3;4;5;6;7;8;9]
-    let ((r,c),s) = ((5,5), vals |> List.zip cells |> Map.ofList)
-    let expectedOutput = 9    
-    let actualOutput = cellSoln r c s
-    Assert.AreEqual(expectedOutput, actualOutput)
-
-[<Test>]
-let ``Test sweep when no solutions`` () =
-    let func = sweep
-    let cells = [(5,1);(5,2);(5,7);(5,9);(1,5);(3,5);(4,5);(8,5);(2,2)]
-    let vals = [1;2;3;4;4;6;7;8;9]
-    let solved = vals |> List.zip cells |> Map.ofList
-    let unsolved = [(1,1);(5,5)] |> Set.ofList
-    let expectedOutput = (solved, unsolved)   
-    let actualOutput = func (solved, unsolved)
-    Assert.AreEqual(expectedOutput, actualOutput)
-
-[<Test>]
-let ``Test sweep when is solution`` () =
-    let func = sweep
-    let cells = [(5,1);(5,2);(5,7);(5,9);(1,5);(3,5);(4,5);(8,5);(2,2)]
-    let vals = [1;2;3;4;5;6;7;8;9]
-    let solved = vals |> List.zip cells |> Map.ofList
-    let unsolved = [(1,1);(5,5)] |> Set.ofList
-    let expectedOutput = (solved |> Map.add (5,5) 9, unsolved |> Set.remove (5,5))   
-    let actualOutput = func (solved, unsolved)
-    Assert.AreEqual(expectedOutput, actualOutput)
-
-[<Test>]
-let ``Test whole puzzle solution`` () =
+let ``Test a valid 9x9 puzzle`` () =
     let puzzleString = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
     let solutionString = "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
+    let mySolutionString = solvePuzzle puzzleString
+    Assert.AreEqual(solutionString, mySolutionString)
+
+[<Test>]
+let ``Test an invalid puzzle`` () =
+    let puzzleString = "0043002090050090010700600430060020871900074000500830006000001050035086900"
+    let solutionString = "Error: Invalid puzzle."
     let mySolutionString = solvePuzzle puzzleString
     Assert.AreEqual(solutionString, mySolutionString)
 
